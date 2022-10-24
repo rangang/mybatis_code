4b5ee74edd5953b4518bf3b29c5b5d6cb6e78ca8
rangang <13452334233@163.com>
2022-10-24 13:16:55 +0800

    Mytatis高级查询，Mytatis映射文件深入

--- mybatis_dao/src/main/java/com/rg/mapper/UserMapper.java
+++ mybatis_dao/src/main/java/com/rg/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.rg.mapper;
 
 import com.rg.domain.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.io.IOException;
 import java.util.List;
@@ -20,6 +21,83 @@ public interface UserMapper {
      */
     public User findById(Integer id);
 
+    /**
+     * 查询所有用户
+     * @return
+     */
+    public List<User> findAllResultMap();
 
+    /**
+     * 多条件查询方式一
+     * @return
+     */
+    public List<User> findByIdAndUsername1(Integer id, String username);
+
+    /**
+     * 多条件查询方式二
+     * @param id
+     * @param username
+     * @return
+     */
+    public List<User> findByIdAndUsername2(@Param("id") Integer id, @Param("username") String username);
+
+    /**
+     * 多条件查询方式三
+     * @param user
+     * @return
+     */
+    public List<User> findByIdAndUsername3(User user);
+
+    /**
+     * 模糊查询：方式一
+     * @param username
+     * @return
+     */
+    public List<User> findByUsername1(String username);
+
+    /**
+     * 模糊查询：方式二
+     * @param username
+     * @return
+     */
+    public List<User> findByUsername2(String username);
+
+    /**
+     * 添加用户，获取返回主键：方式一
+     * @param user
+     */
+    public void saveUser(User user);
+
+    /**
+     * 添加用户，获取返回主键：方式二
+     * @param user
+     */
+    public void saveUser2(User user);
+
+    /**
+     * 动态SQL之if:多条件查询
+     * @return
+     */
+    public List<User> findByIdAndUsernameIf(User user);
+
+    /**
+     * 动态SQL之set:动态更新
+     * @param user
+     */
+    public void updateIf(User user);
+
+    /**
+     * 动态SQL之foreach:多值查询：集合
+     * @param ids
+     * @return
+     */
+    public List<User> findByList(List<Integer> ids);
+
+    /**
+     * 动态SQL之foreach：多值查询：数组
+     * @param ids
+     * @return
+     */
+    public List<User> findByArray(Integer[] ids);
 
 }
--- mybatis_dao/src/main/java/com/rg/test/MybatisTest.java
+++ mybatis_dao/src/main/java/com/rg/test/MybatisTest.java
@@ -1,5 +1,7 @@
 package com.rg.test;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.rg.domain.User;
 import com.rg.mapper.UserMapper;
 import org.apache.ibatis.io.Resources;
@@ -10,6 +12,8 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -39,7 +43,296 @@ public class MybatisTest {
         User user = userMapper.findById(1);
         System.out.println(user);
 
+        // 释放资源
+        sqlSession.close();
 
+    }
+
+    @Test
+    public void testFindAllResultMap() throws IOException {
+
+        // 加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 获取SqlSessionFactory工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获取SqlSession会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 基于UserMapper所产生的代理对象，底层：JDK动态代理 实际类型：proxy
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = userMapper.findAllResultMap();
+        // 打印
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        // 释放资源
+        sqlSession.close();
+
+    }
+
+    @Test
+    public void testFindByIdAndUsername1() throws IOException {
+
+        // 加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 获取SqlSessionFactory工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获取SqlSession会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 基于UserMapper所产生的代理对象，底层：JDK动态代理 实际类型：proxy
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = userMapper.findByIdAndUsername1(5, "rg");
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        // 释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void testFindByIdAndUsername2() throws IOException {
+        // 加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 获取SqlSessionFactory工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获取SqlSession会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 基于UserMapper所产生的代理对象，底层：JDK动态代理，实际类型：proxy
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = userMapper.findByIdAndUsername2(5, "rg");
+        for (User user : users) {
+            System.out.println(user);
+        }
+        // 释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void testFindByIdAndUsername3() throws IOException {
+        // 加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 获取SqlSessionFactory工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获取SqlSession会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 基于UserMapper所产生代理对象，底层：JDK动态代理，实际类型：proxy
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        User user = new User();
+        user.setId(5);
+        user.setUsername("rg");
+        List<User> users = userMapper.findByIdAndUsername3(user);
+        for (User user1 : users) {
+            System.out.println(user1);
+        }
+        // 释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void testFindByUsername() throws IOException {
+        // 加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 获取SqlSessionFactory工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获取SqlSession会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 基于UserMapper所产生的代理对象，底层：JDK动态代理，实际类型：proxy
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = userMapper.findByUsername1("%r%");
+        for (User user : users) {
+            System.out.println(user);
+        }
+        // 释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void testFindByUsername2() throws IOException {
+        // 加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 获取SqlSessionFactory工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获取SqlSession会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 基于UserMapper所产生的代理对象，底层：JDK动态代理，实际类型：proxy
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = userMapper.findByUsername2("%r%");
+        for (User user : users) {
+            System.out.println(user);
+        }
+        // 释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void testSaveUser() throws IOException {
+        // 加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 获取SqlSessionFactory工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获取SqlSession会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 基于UserMapper所产生的代理对象，底层：JDK动态代理，实际类型：proxy
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        User user = new User();
+        user.setUsername("testName");
+        user.setBirthday(new Date());
+        user.setSex("女");
+        user.setAddress("cd");
+        userMapper.saveUser(user);
+        System.out.println(user);
+
+        // 手动提交事务
+        sqlSession.commit();
+        // 释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void testSaveUser2() throws IOException {
+        // 加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 获取SqlSessionFactory工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获取SqlSession会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 基于UserMapper所产生的代理对象，底层：JDK动态代理，实际类型：proxy
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        User user = new User();
+        user.setUsername("测试1");
+        user.setBirthday(new Date());
+        user.setSex("男");
+        user.setAddress("sz");
+
+        userMapper.saveUser2(user);
+        System.out.println(user);
+
+        // 手动提交事务
+        sqlSession.commit();
+        // 释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void testFindByIdAndUsername() throws IOException {
+        // 加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 获取SqlSessionFactory工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获取SqlSession会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 基于UserMapper产生代理对象，底层：JDK动态代理，实际类型：proxy
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        User user = new User();
+        // user.setId(5);
+        // user.setUsername("rg");
+        List<User> users = userMapper.findByIdAndUsernameIf(user);
+        for (User user1 : users) {
+            System.out.println(user1);
+        }
+
+        // 释放资源
+        sqlSession.close();
+
+    }
+
+    @Test
+    public void testUpdateIf() throws IOException {
+
+        // 加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 获取SqlSessionFactory工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获取SqlSession会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 基于UserMapper所产生的代理对象，底层：JDK动态代理，实际类型：proxy
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        User user = new User();
+        user.setId(8);
+        user.setUsername("RRG");
+        userMapper.updateIf(user);
+
+        // 手动提交事务
+        sqlSession.commit();
+        // 释放资源
+        sqlSession.close();
+
+    }
+
+    @Test
+    public void testFindByList() throws IOException {
+        // 加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 获取SqlSessionFactory工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获取SqlSession会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        ArrayList<Integer> ids = new ArrayList<>();
+        ids.add(1);
+        ids.add(3);
+        ids.add(5);
+        List<User> users = userMapper.findByList(ids);
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        // 释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void testFindByArray() throws IOException {
+        // 加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 获取SqlSessionFactory工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获取SqlSession会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        Integer[] ids = {2,6,8};
+        List<User> users = userMapper.findByArray(ids);
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        // 释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void testPageHelper() throws IOException {
+        // 加载核心配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        // 获取SqlSessionFactory工厂对象
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        // 获取SqlSession会话对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        // 设置分页参数
+        PageHelper.startPage(1,2);
+
+        List<User> users = userMapper.findAllResultMap();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        PageInfo<User> pageInfo = new PageInfo<>(users);
+        System.out.println(pageInfo);
+
+        // 释放资源
+        sqlSession.close();
     }
 
 }
